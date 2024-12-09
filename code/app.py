fc16be3b2dc85e63a137d949966f62f011032f28
Asif Sayyed <asifdotexe@gmail.com>
2024-12-09 22:58:35 +0530

    #1 updated the relative path

--- code/app.py
+++ code/app.py
@@ -17,7 +17,7 @@ def main():
     )
 
     # Load the trained model
-    model = load_trained_model("../saved_model/mnist_digit_recognizer.keras")
+    model = load_trained_model(r"..\saved_model\mnist_digit_recognizer.keras")
 
     # File uploader to upload an image
     uploaded_file = st.file_uploader("Upload your image", type=["png", "jpg", "jpeg"])
